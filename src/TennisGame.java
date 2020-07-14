c459590fd8fdfe9274cd5cdb57ca4f079b7dd330
hongquan16101995 <66354589+hongquan16101995@users.noreply.github.com>
2020-07-14 14:54:49 +0700

    suaclassTennisGame

--- src/TennisGame.java
+++ src/TennisGame.java
@@ -1,60 +1,92 @@
 public class TennisGame {
 
-    public static String getScore(String player1Name, String player2Name, int m_score1, int m_score2) {
+    public static String getScore(String player1Name, String player2Name, int player1_score, int player2_score) {
         String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                    score = "Love-All";
-                    break;
-                case 1:
-                    score = "Fifteen-All";
-                    break;
-                case 2:
-                    score = "Thirty-All";
-                    break;
-                case 3:
-                    score = "Forty-All";
-                    break;
-                default:
-                    score = "Deuce";
-                    break;
-
-            }
+        if (player1_score == player2_score) {
+            score = getString(player1_score);
         }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+        else if (player1_score >= 4 || player2_score >= 4) {
+            score = getStringWin(player1_score, player2_score);
+        }
+        else {
+            score = getString2(player1_score, player2_score, score);
+        }
+        return score;
+    }
+
+
+    private static String getString2(int player1_score, int player2_score, String score) {
+        int tempScore;
+        for (int i = 1; i < 3; i++) {
+            if (i == 1) {
+                tempScore = player1_score;
+            }
+            else {
+                score += "-";
+                tempScore = player2_score;
+            }
+
+            score = getStringByTempScore(score, tempScore);
+        }
+        return score;
+    }
+
+
+    private static String getStringByTempScore(String score, int tempScore) {
+        switch(tempScore) {
+            case 0:
+                score += "Love";
+                break;
+            case 1:
+                score += "Fifteen";
+                break;
+            case 2:
+                score += "Thirty";
+                break;
+            case 3:
+                score += "Forty";
+                break;
+        }
+        return score;
+    }
+
+
+    private static String getStringWin(int player1_score, int player2_score) {
+        String score;
+        int minusResult = player1_score - player2_score;
+        if (minusResult==1) {
+            score = "Advantage player1";
+        } else if (minusResult ==-1) {
+            score = "Advantage player2";
+        }
+        else if (minusResult>=2) {
+            score = "Win for player1";
         }
         else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
+            score = "Win for player2";
+        return score;
+    }
+
+
+    private static String getString(int player1_score) {
+        String score;
+        switch (player1_score) {
+            case 0:
+                score = "Love-All";
+                break;
+            case 1:
+                score = "Fifteen-All";
+                break;
+            case 2:
+                score = "Thirty-All";
+                break;
+            case 3:
+                score = "Forty-All";
+                break;
+            default:
+                score = "Deuce";
+                break;
+
         }
         return score;
     }
